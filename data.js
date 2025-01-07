ed298d9b31a3c3f3e557e0989f0973ee88da7ede
Maestroprosiak <132168443+Maestroprosiak@users.noreply.github.com>
2025-01-07 15:35:04 +0100

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const movies = [
         description: "Description",
         rating: 53,  // Note TMDb
         ageRating: "TP",  // Âge minimum
-        thumbnail: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/exemple.jpg",  // Image depuis TMDb
+        thumbnail: "Arcane.jpg",  // Image depuis TMDb
         videoUrl: "https://github.com/Maestroprosiak/losoweslowa/blob/44e8e211ed68b8f98c3b708f9b14238c741c2c52/Film%20bez%20tytu%C5%82u%20%E2%80%90%20Wykonano%20za%20pomoc%C4%85%20Clipchamp.mp4"
     }
 ];
