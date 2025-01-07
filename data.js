2dc09dd44c0740309edf9a1cdedf742ea105b3be
Maestroprosiak <132168443+Maestroprosiak@users.noreply.github.com>
2025-01-07 19:49:11 +0100

    Update data.js

--- data.js
+++ data.js
@@ -167,6 +167,105 @@ const series = [
                     }
                 ]
             },
+                {
+        title: "薬屋のひとりごと",
+        description: "Zielarka",
+        rating: 95,
+        ageRating: "TV-8",
+        thumbnail: "images/apothecary.webp",
+        seasons: [
+            {
+                seasonNumber: 1,
+                episodes: [
+                    {
+                        title: "Odcinek 1",
+                        videoUrl: "https://vk.com/video_embed.php?oid=858592511&id=456293845&hash=d541a3285f07304f",
+                        isEmbed: true // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 2",
+                        videoUrl: "https://vk.com/video_embed.php?oid=858592511&id=456293977&hash=606a4a62db576d95",
+                        isEmbed: true // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 3",
+                        videoUrl: "https://vk.com/video_ext.php?oid=858592511&id=456242286&hash=f3f74e3473ff2cf6",
+                        isEmbed: true // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 4",
+                        videoUrl: "https://mega.nz/embed/N6EygR7J#9sv8ucgQB9hKdsTRf2O4HvYAHEKvH-WVB7NhsZa-CLk",
+                        isEmbed: true // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 5",
+                        videoUrl: "https://vider.info/embed/video/ss15vc",
+                        isEmbed: true // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 6",
+                        videoUrl: "https://vider.info/embed/video/ss15vv",
+                        isEmbed: true // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 7",
+                        videoUrl: "https://vider.info/embed/video/ss1cmm",
+                        isEmbed: true // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 8",
+                        videoUrl: "https://vider.info/embed/video/ss1cmn",
+                        isEmbed: true // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 9",
+                        videoUrl: "https://vider.info/embed/video/ss1cxc",
+                        isEmbed: true // Dodano pole isEmbed
+                    }
+                ]
+            },
+            {
+                seasonNumber: 2,
+                episodes: [
+                    {
+                        title: "Odcinek 1",
+                        videoUrl: "https://vider.info/embed/video/ss1nxm",
+                        isEmbed: true // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 2",
+                        videoUrl: "https://vider.info/embed/video/ss1nxn",
+                        isEmbed: true // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 5",
+                        videoUrl: "https://link-to-video.com",
+                        isEmbed: false // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 6",
+                        videoUrl: "https://link-to-video.com",
+                        isEmbed: false // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 7",
+                        videoUrl: "https://link-to-video.com",
+                        isEmbed: false // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 8",
+                        videoUrl: "https://link-to-video.com",
+                        isEmbed: false // Dodano pole isEmbed
+                    },
+                    {
+                        title: "Odcinek 8",
+                        videoUrl: "https://link-to-video.com",
+                        isEmbed: false // Dodano pole isEmbed
+                    }
+                ]
+            }
+        ]
+    },
         ]
     },
 ];
