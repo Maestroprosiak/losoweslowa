708d2725d334729b8ca1a69ee5e1a2c0c3491fc3
Maestroprosiak <132168443+Maestroprosiak@users.noreply.github.com>
2025-01-07 15:57:02 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,31 +1,84 @@
 const movies = [
     {
         title: "Arcane",
-        description: "Specjalnie dla ciebie Kamilku UwU",
-        rating: 53,  // Note TMDb
-        ageRating: "TP",  // Âge minimum
-        thumbnail: "Arcane.jpg",  // Image depuis TMDb
-        videoUrl: "Arcane.mp4"
+        description: "Dwie siostry się napierdalają",
+        rating: 88,  
+        ageRating: "TP",  
+        thumbnail: "https://preview.redd.it/s1-spoilers-mystery-netflix-thumbnails-v0-x3rrb6umgiea1.jpg?width=356&format=pjpg&auto=webp&s=9a129df360bb733b448539187bb660b2598d17d9",  // Image depuis TMDb
+        videoUrl: "https://streamable.com/qc719u"
     }
 ];
 
 const series = [
     {
-        title: "Serie exemple 1",
-        description: "Description",
-        rating: 78,
+        title: "Arcane",
+        description: "Kamilku to dla ciebie UwU",
+        rating: 88,
         ageRating: "TV-14",
         thumbnail: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/exemple.jpg",
         seasons: [
             {
-                seasonNumber: 9,
+                seasonNumber: 1,
                 episodes: [
                     {
-                        title: "Episod 1",
+                        title: "Odcinek 1",
                         videoUrl: "https://link-to-video.com"
                     },
                     {
-                        title: "Episod 2",
+                        title: "Odcinek 2",
+                        videoUrl: "https://link-to-video.com"
+                    },
+                    {
+                        title: "Odcinek 5",
+                        videoUrl: "https://link-to-video.com"
+                    },
+                    {
+                        title: "Odcinek 6",
+                        videoUrl: "https://link-to-video.com"
+                    },
+                    {
+                        title: "Odcinek 7",
+                        videoUrl: "https://link-to-video.com"
+                    },
+                    {
+                        title: "Odcinek 8",
+                        videoUrl: "https://link-to-video.com"
+                    },
+                    {
+                        title: "Odcinek 8",
+                        videoUrl: "https://link-to-video.com"
+                    }
+                ]
+            },
+            {
+                seasonNumber: 2,
+                episodes: [
+                    {
+                        title: "Odcinek 1",
+                        videoUrl: "https://link-to-video.com"
+                    },
+                    {
+                        title: "Odcinek 2",
+                        videoUrl: "https://link-to-video.com"
+                    },
+                    {
+                        title: "Odcinek 5",
+                        videoUrl: "https://link-to-video.com"
+                    },
+                    {
+                        title: "Odcinek 6",
+                        videoUrl: "https://link-to-video.com"
+                    },
+                    {
+                        title: "Odcinek 7",
+                        videoUrl: "https://link-to-video.com"
+                    },
+                    {
+                        title: "Odcinek 8",
+                        videoUrl: "https://link-to-video.com"
+                    },
+                    {
+                        title: "Odcinek 8",
                         videoUrl: "https://link-to-video.com"
                     }
                 ]
